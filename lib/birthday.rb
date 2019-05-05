db40aeac51ec4b6884b5106bed290799abe53901
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-05 02:54:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ end
 
 def age_appropriate_birthday(birthdays)
   birthdays.each do |kids_name, age|
-    if(age < 12) do 
+    if(age < 12) 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else 
       puts "You are too old for this."
